670bdaba80eda2cc5ced354d13f1ba2397da7982
Vinanti <vinantinaik@gmail.com>
2018-03-12 22:43:31 -0500

    more styling

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -18,65 +18,65 @@ $(document).ready(function () {
     db.ref().on('value', function (snapshot) {
 
         var data = snapshot.val();
-		var timeDiff = 0;
-		var nextTrain ="";
-		var remainingTime = 0;
-		var timeToArrive="";
+        var timeDiff = 0;
+        var nextTrain = "";
+        var remainingTime = 0;
+        var timeToArrive = "";
         $("tbody").empty();
-        $.each(data,function(key,value){
-			
-			var a = moment.unix(value.firstTrain);
-					
-			var b = moment();
-			
-				
-			timeDiff = b.diff(a,"minutes");
-			remainingTime=  timeDiff % value.frequency;
-			
-			timeToArrive = value.frequency - remainingTime;
-			
-			nextTrain = moment().add(timeToArrive,"m").format("hh:mm: A");
+        $.each(data, function (key, value) {
+
+            var a = moment.unix(value.firstTrain);
+
+            var b = moment();
+
+
+            timeDiff = b.diff(a, "minutes");
+            remainingTime = timeDiff % value.frequency;
+
+            timeToArrive = value.frequency - remainingTime;
+
+            nextTrain = moment().add(timeToArrive, "m").format("hh:mm: A");
 
             var trTag = $("<tr>");
+            trTag.addClass("trainRow");
             var thTag = $("<th>");
             thTag.attr("scope", "row");
             thTag.html(key);
             trTag.append(thTag);
             var tdTag1 = $("<td>");
             tdTag1.html(value.destination);
-    
+
             var tdTag2 = $("<td>");
             tdTag2.html(value.frequency);
-    
+
             var tdTag3 = $("<td>");
             tdTag3.html(nextTrain);
-    
+
             var tdTag4 = $("<td>");
             tdTag4.html(timeToArrive);
-    
+
             trTag.append(tdTag1, tdTag2, tdTag3, tdTag4);
-    
+
             $("tbody").append(trTag);
 
         })
 
+
+
        
-
-        getNextTrainTime(data.firstTrain);
-
     });
 
     $("#submit").on("click", function (event) {
         event.preventDefault();
-        
-        
+
+
 
         var trainName = $("#input-train-name").val().trim();
         train.destination = $("#input-train-destination").val().trim();
-        train.firstTrain = moment($("#input-train-time").val().trim(),"HH:mm").subtract(1, "years").format("X");
+        train.firstTrain = moment($("#input-train-time").val().trim(), "HH:mm").subtract(1, "years").format("X");
         train.frequency = $("#input-train-frequency").val().trim();
 
-        var trainObj ={
+        var trainObj = {
             [trainName]: train
 
         };
@@ -88,30 +88,16 @@ $(document).ready(function () {
 
 
     });
-    // getTrainSchedules();
 
-    // function getTrainSchedules() {
-    //     var data = snapshot.val();
-    // }
+    $("span").hide();
+    $("tr").hover(
+        function () {
+            $(this).find("span").show();
+        },
+        function () {
+            $(this).find("span").hide();
+        });
 
-    function getNextTrainTime(firstTrainTime) {
-
-       
-
-        var time = new Date();
-
-        var timeStart = new Date(time.getDate().toString() + " " +firstTrainTime) ;
-
-        var diff = (time - timeStart)/60000;
-
-        var minutes = diff % 60;
-
-        var hours = (diff- minutes)/60;
-        console.log(
-            time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-        );
-        
-
-    }
+  
 
 });
